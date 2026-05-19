ae65db1e5559551c03a7f8394bdce2503fb9c514
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2026-05-19 20:57:18 -0300

    chore: finaliza a configuração inicial do projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import '../global.css';
+
+export default function Index() {
+  return (
+    <View className="grow items-center justify-center">
+      <Text>Olá Mundo!</Text>
+    </View>
+  );
+}
