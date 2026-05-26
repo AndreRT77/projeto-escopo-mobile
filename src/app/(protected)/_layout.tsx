730aeb03ef143ee362019d6cbd075146e59b0046
NATHAN DA SILVA COSTA <25132656@CFP127ALUNO.LOCAL>
2026-05-26 09:42:31 -0300

    feat: implementa logo e botão de abrir/fechar menu

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,10 +1,14 @@
 import { Redirect, Stack } from 'expo-router'
-import { ActivityIndicator } from 'react-native'
+import { ActivityIndicator, Image, TouchableOpacity } from 'react-native'
 
+import LogoImg from '@/assets/images/logo-white.png'
 import { useAuth } from '@/hooks/useAuth'
+import { Menu, X } from 'lucide-react-native'
+import { useState } from 'react'
 
 export default function ProtectedLayout() {
   const { isLoggedIn, isReady } = useAuth()
+  const [menuOpen, setMenuOpen] = useState(false)
 
   if (!isReady) {
     return <ActivityIndicator className="grow items-center justify-center" />
@@ -15,8 +19,20 @@ export default function ProtectedLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="dashboard" options={{ title: 'Dashboard' }} />
-    </Stack>
+    <Stack
+      screenOptions={{
+        title: '',
+        headerStyle: {
+          backgroundColor: '#552BA9',
+        },
+        headerLeft: () => <Image source={LogoImg} resizeMode="contain" className="h-12 w-44" />,
+        headerRight: () => (
+          <TouchableOpacity onPress={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? <X color="white" /> : <Menu color="white" />}
+          </TouchableOpacity>
+        ),
+        headerBackVisible: true,
+      }}
+    ></Stack>
   )
 }
